575edf336662f7ba3cb21954ab72183ef6ea8c6b
Dennis Reimann <mail@dennisreimann.de>
2021-04-27 10:12:37 +0200

    🆙 Upgrade dependencies

--- postcss/main/postcss.config.js
+++ postcss/main/postcss.config.js
@@ -1,7 +1,7 @@
 const autoprefixer = require('autoprefixer')
 const extend = require('postcss-extend')
 const importer = require('postcss-import')
-const nesting = require('postcss-nesting')
+const nesting = require('postcss-nesting').default
 const prettify = require('postcss-prettify')
 
 module.exports = {
