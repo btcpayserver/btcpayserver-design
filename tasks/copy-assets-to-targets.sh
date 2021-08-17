9a40270ebaa6f9af45f9a609282f7a1260e8d100
Dennis Reimann <mail@dennisreimann.de>
2021-08-17 11:14:53 +0200

    Add scripts to copy assets task

--- tasks/copy-assets-to-targets.sh
+++ tasks/copy-assets-to-targets.sh
@@ -2,6 +2,6 @@
 
 if [ -d "$BTCPAY_TARGET" ]; then
   cp dist/styles/btcpayserver-bootstrap.css "$BTCPAY_TARGET/BTCPayServer/wwwroot/main/bootstrap/bootstrap.css"
-
+  cp dist/scripts/bootstrap/* "$BTCPAY_TARGET/BTCPayServer/wwwroot/vendor/bootstrap/"
   echo "Copied assets to $BTCPAY_TARGET"
 fi
