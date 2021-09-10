269563b3737c38351b078d822fffdc4c167895f3
Dennis Reimann <mail@dennisreimann.de>
2021-09-10 15:31:53 +0200

    Improve assets copy task

--- tasks/copy-assets-to-targets.sh
+++ tasks/copy-assets-to-targets.sh
@@ -3,5 +3,20 @@
 if [ -d "$BTCPAY_TARGET" ]; then
   cp dist/styles/btcpayserver-bootstrap.css "$BTCPAY_TARGET/BTCPayServer/wwwroot/main/bootstrap/bootstrap.css"
   cp dist/scripts/bootstrap/* "$BTCPAY_TARGET/BTCPayServer/wwwroot/vendor/bootstrap/"
+
+  # Update default.css
+  rootSelector=":root {[\s\S]*?}\n"
+  defaultCSS="$BTCPAY_TARGET/BTCPayServer/wwwroot/main/themes/default.css"
+  defaultRoot=$(cat dist/styles/btcpayserver-variables.css | grep -oPz "$rootSelector")
+  perl -0777 -i -pe "s/$rootSelector//" $defaultCSS
+  echo -e "$defaultRoot\n$(cat $defaultCSS)" > $defaultCSS
+
+  # Update default-dark.css
+  rootDarkSelector=":root\[data-btcpay-theme=\"dark\"\] {[\s\S]*?}\n"
+  defaultDarkCSS="$BTCPAY_TARGET/BTCPayServer/wwwroot/main/themes/default-dark.css"
+  defaultDarkRoot=$(cat dist/styles/btcpayserver-variables.css | grep -oPz "$rootDarkSelector" | sed "s/\[data-btcpay-theme=\"dark\"\]//g")
+  perl -0777 -i -pe "s/$rootSelector//" $defaultDarkCSS
+  echo -e "$defaultDarkRoot\n$(cat $defaultDarkCSS)" > $defaultDarkCSS
+
   echo "Copied assets to $BTCPAY_TARGET"
 fi
