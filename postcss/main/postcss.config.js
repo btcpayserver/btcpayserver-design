eacd98653ca8be11af16f2466649cf63af29e068
Dennis Reimann <mail@dennisreimann.de>
2022-07-19 19:29:02 +0200

    Upgrade dependencies

--- postcss/main/postcss.config.js
+++ postcss/main/postcss.config.js
@@ -1,17 +1,17 @@
 const autoprefixer = require('autoprefixer')
-const extend = require('postcss-extend')
+const mixins = require("postcss-mixins");
 const importer = require('postcss-import')
 const nesting = require('postcss-nesting')
 const prettify = require('postcss-prettify')
 
 module.exports = {
   plugins: [
+    mixins(),
     autoprefixer(),
     importer({
-      path: ['src/styles', 'src/components'],
+      path: ["src/styles", "src/components"],
     }),
-    extend(),
     nesting(),
-    prettify()
-  ]
-}
+    prettify(),
+  ],
+};
